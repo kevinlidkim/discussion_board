e6543ce4e940bd9bfa839498b8733b9c88b61ee0
kevin <kevin.li.3@stonybrook.edu>
2016-11-29 23:14:16 -0500

    changed code

--- server.py
+++ server.py
@@ -7,37 +7,68 @@
 from socket import *
 from threading import *
 from SocketServer import *
+from select import *
+from sys import *
+
+class Server:
+
+    def __init__(self):
+        self.port = 7727
+        self.threads = []
+        self.server = None
+        self.host = ""
+
+    def open_socket(self):
+        try:
+            self.server = socket(AF_INET, SOCK_STREAM)
+            self.server.bind((self.host, self.port))
+            self.server.listen(5)
+        except socket.error, (value, message):
+            if self.server:
+                self.server.close()
+            print "Could not open socket: " + message
+            sys.exit(1)
+
+    def run(self):
+        self.open_socket()
+        input = [self.server, sys.stdin]
+        running = 1
+        while running:
+            inputready, outputready, exceptready = select(input, [], [])
+
+            for s in inputready:
+                if s == self.server:
+                    c = ClientThread(self.server.accept())
+                    c.start()
+                    self.threads.append(c)
+
+                elif s == sys.stdin:
+                    junk = sys.stdin.readline()
+                    running = 0
+
+        self.server.close()
+        for c in self.threads:
+            c.join()
+                
 
 class ClientThread(Thread):
 
-    def __init__(self, ip, port):
+    def __init__(self, (ip, port)):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         print ("[+] New server socket thread started for " + ip + ":" + str(port))
 
     def run(self):
-        while True:
-            data = conn.recv(2048)
-            print "Server received data: ", data
-            MESSAGE = raw_input("Multithreaded Python server : Enter reponse from server:")
-            if MESSAGE == 'exit':
-                break
-            conn.send(MESSAGE)
+        running = 1
+        while running:
+            data = self.client.recv(1024)
+            if data:
+                self.client.send(data)
+            else:
+                self.client.close()
+                running = 0
 
-serverPort = 7727
-server = socket(AF_INET, SOCK_STREAM)
-server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-server.bind(("", serverPort))
-threads = []
-
-while True:
-    server.listen(4)
-    print "Multithreaded Python server : Waiting for connections from clients..."
-    (conn, (ip,port)) = server.accept()
-    newThread = ClientThread(ip, port)
-    newThread.start()
-    threads.append(newThread)
-
-for t in threads:
-    t.join()
+if __name__ == "__main__":
+    s = Server()
+    s.run()
